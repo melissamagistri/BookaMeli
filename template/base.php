408c27d98efb1c17674ee6ea7752ce74eb08906b
Melissa Magistri <melissamagistri@192.168.1.67>
2021-12-19 18:13:43 +0100

    create cartelle e file base

---
+++ template/base.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $templateParams["titolo"]; ?></title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <?php //carica img cubi, carrello e menubar 
+    if(isset($templateParams["js"])):
+        foreach($templateParams["js"] as $script):
+    ?>
+        <script src="<?php echo $script; ?>"></script>
+    <?php
+        endforeach;
+    endif;
+    ?>
+</head>
+<body>
+    <header> 
+        
+    </header>
+</body>
+</html>
