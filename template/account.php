395b5acc306770d5f6f1bf08775c63169f5b943e
Melissa Magistri <melissamagistri@192.168.1.67>
2021-12-28 23:09:42 +0100

    creata la pagina account

---
+++ template/account.php
@@ -0,0 +1,16 @@
+<section>
+    <h1>Accedi al tuo account</h1>
+    <div>
+    <form action="/action_page.php">
+    <input type="text" id="email" name="firstname" placeholder="Email">
+
+    <input type="text" id="password" name="lastname" placeholder="Password">
+  
+    <input type="submit" value="Accedi">
+  </form>
+</div>
+
+<div>
+  <a href="creaaccount"> Crea un account</a>
+</div>
+</section>
