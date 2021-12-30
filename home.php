b3bda20a66d8563018ed47e50cd53a451a25af9b
Federico <federico.guidazzi@studio.unibo.it>
2021-12-30 14:09:00 +0100

    remove home.php

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'util.php';
-
-$templateParams["titolo"] = "BookaMeli - HomePage";
-$templateParams["nome"] = "search.php";
-$templateParams["js"] = array("js/jquery-3.4.1.min.js","js/baseScript.js");
-
-require_once 'template/base.php';
-?>
