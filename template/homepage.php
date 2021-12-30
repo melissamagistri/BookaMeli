b8bf18a18846d757e7f574981c3c023323627c74
Melissa Magistri <melissamagistri@192.168.1.67>
2021-12-30 12:24:32 +0100

    aggiunte liste home

--- template/homepage.php
+++ template/homepage.php
@@ -2,7 +2,75 @@
    <div class="search">
       <input type="text" class="searchTerm" placeholder="Che cosa stai cercando?">
       <button type="submit" class="searchButton">
-        <i class="fa fa-search"></i>
+        <span class="fa fa-search"></span>
      </button>
    </div>
 </div>
+
+<div>
+  <a href="">
+    <img src="" alt="">
+  </a>
+</div>
+
+<div>
+  <div class="dot">
+    <span></span>
+  </div>
+</div>
+
+
+<section>
+  <h2>Novità</h2>
+  <div>
+      <ul>
+        <li><img class="imghome" src="<?php echo UPLOAD_DIR.'onepiece1.png'?>" alt="one-piece-vol-1"></li>
+        <li><a href="">Titolo</a></li>
+        <li><p>Prezzo</p></li>
+        <li>
+          <div>
+              <a class="bluebutton" href="">Aggiungi al carrello</a>
+          </div>
+        </li>
+      </ul>
+
+    </div>   
+
+    <div>
+      <div class="dot">
+       <span></span>
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
+<section>
+  <h2>Popolari</h2>
+  <div>
+    <ul>
+        <li><img class="imghome" src="<?php echo UPLOAD_DIR.'onepiece1.png'?>" alt="one-piece-vol-1"></li>
+        <li><a href="">Titolo</a></li>
+        <li><p>Prezzo</p></li>
+        <li>
+          <div>
+              <a class="bluebutton" href="">Aggiungi al carrello</a>
+          </div>
+        </li>
+    </ul>
+  </div>
+
+    <div>
+      <div class="dot">
+       <span></span>
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
+
+
+
