d018bc790f00eb221ea13f685011f5541a83c5c0
Federico <federico.guidazzi@studio.unibo.it>
2021-12-31 17:22:21 +0100

    aggiunto il file js per la gestione della pagina registrati

--- registrati.php
+++ registrati.php
@@ -3,7 +3,7 @@ require_once 'util.php';
 
 $templateParams["titolo"] = "BookaMeli - Registrati";
 $templateParams["nome"] = "template/registrati.php";
-$templateParams["js"] = array("js/jquery-3.4.1.min.js","js/baseScript.js");
+$templateParams["js"] = array("js/jquery-3.4.1.min.js","js/baseScript.js", "js/registrati.js");
 
 require_once 'template/base.php';
 ?>
