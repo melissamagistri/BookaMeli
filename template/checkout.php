96159ca12b94068c282db8af99a556fb8fbbca1d
Melissa Magistri <melissamagistri@192.168.1.67>
2022-01-01 15:11:20 +0100

    aggiunto lo stile di checkout

--- template/checkout.php
+++ template/checkout.php
@@ -2,8 +2,11 @@
     <div >
         <ul>
             <li><h1>1.Informazioni Personali</h1></li>
-            <li><p>Collegato come: <a href=""></a></p></li>
-            <li><p>Cambia account: <a href=""></a></p></li>
+            <li><p>Collegato come: <a href=""></a> <label for="">Nome cognome</label></p></li>
+            <li><p>Cambia account <a href=""></a></p></li>
+            <li><input type="text" placeholder="Email"></li>
+            <li><input type="text" placeholder="Password"></li>
+            <li><button class="bluebutton">Invia</button></li>
             <li><button class="bluebutton">Continua</button></li>
             <li><span class="fas fa-angle-down"></span></li>
         </ul>
@@ -15,19 +18,20 @@
         <ul>
             <li><h1>2.Pagamento</h1></li>
             <li>
-                <input type="radio" id="contanti" >
+                <form action="">
+                <input type="radio" id="contanti" name="contanti" value="Contanti">
                 <label for="contanti">Contanti</label>
-                <input type="radio" id="carta" name="carta">
+                <input type="radio" id="carta" name="carta" value="Carta di credito">
                 <label for="carta">Carta di credito</label>
+                
             </li>
 
             <li>
-                <h3>Inserisci i dati della tua carta</h3>
+            <h3>Inserisci i dati della tua carta</h3>
                 <ul>
-                    <li>
-                        <input type="text" placeholder="Numero">
-                        <input type="text" placeholder="Proprietario">
-                    </li>
+                    <li><input type="text" placeholder="Numero"></li>
+                    <li><input type="text" placeholder="Proprietario"></li>
+                    
                 </ul>
             </li>
             <li><button class="bluebutton">Continua</button></li>
