d4d65b8c1ccaae1b29287008ce1fe023ed62e6f4
Melissa Magistri <melissamagistri@192.168.1.67>
2022-01-01 15:27:33 +0100

    aggiunta schermata d iricerca con i prodotti

---
+++ template/ricerca.php
@@ -0,0 +1,24 @@
+<section>
+    <div>
+        <a href="">Rilevanza</a>
+        <span class="fas fa-angle-down"></span>
+        <div>
+            <a href="">Dalla A alla Z</a>
+            <a href="">Dal piu recente</a>
+        </div>
+
+        <p>Prodotti visualizzati: <label for="">num prodotti</label></p>
+    </div>
+
+    <div>
+
+    <img src="" alt="">
+    <div>
+        <ul>
+            <li><a href="">titolo</a></li>
+            <li><p>prezzo</p></li>
+            <li><a href="" class="bluebutton">Aggiugi al carrello</a></li>
+        </ul>
+    </div>
+    </div>
+</section>
