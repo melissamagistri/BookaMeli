0adde4ea4a1965afe20b4351be6a314d23b26b20
Melissa Magistri <melissamagistri@192.168.1.61>
2022-01-30 21:16:41 +0100

    gestine prodotti

---
+++ template/venditore/modifica-prodotto.php
@@ -0,0 +1,15 @@
+<section>
+    <div>
+        <img class="imgprodotto" src="<?php echo UPLOAD_DIR.'onepiece1.png'?>" alt="one-piece-vol-1">
+    </div>
+
+    <div>
+        <ul>
+            <li> <input type="text" class="searchTerm" placeholder="Titolo"></li>
+            <li> <input type="text" class="searchTerm" placeholder="Descrizione"></li>
+            <li> <input type="text" class="searchTerm" placeholder="Prezzo"></li>
+            <li><button class="bluebutton">Salva</button></li>
+        </ul>
+    </div>
+    
+</section>
---
+++ template/venditore/prodotto-azioni.php
@@ -0,0 +1,23 @@
+<section>
+    <div>
+        <img class="imgprodotto" src="<?php echo UPLOAD_DIR.'onepiece1.png'?>" alt="one-piece-vol-1">
+    </div>
+
+    <div>
+        <ul>
+            <li><h1>Titolo</h1></li>
+            <li><p>Descrizione</p></li>
+            <li><p>Prezzo</p></li>
+        </ul>
+
+        <ul>
+            <li>
+                <button class="bluebutton" >Elimina</button>
+            </li>
+            <li>
+                <button class="bluebutton" >Modifica</button>
+            </li>
+        </ul>
+    </div>
+    
+</section>
