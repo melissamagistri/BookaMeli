03cbd0e2534b351824fbff6bd80bb6a77cdc1fff
Melissa Magistri <melissamagistri@MacBook-Air-di-Melissa.local>
2022-03-11 14:55:56 +0100

    Cambiato stile delle sezioni dell'account

--- template/info-account.php
+++ template/info-account.php
@@ -2,23 +2,23 @@
     <h1>Chat e Notifiche</h1>
 
     <div class="vertical-items">
-        <ul class="center-ul">
+        <ul class="no-padding">
 
-            <li class="menu-chat-notifiche-margin">
-               <a href="chat-home.php">
+            <li>
+               <a href="chat-notifiche.php">
                <img class="imgdatichat" src="<?php echo UPLOAD_DIR.'bubble_chat.png'?>" alt="chat">
                 <p>Chat e Notifiche</p>
                </a>
             </li>
 
-            <li class="menu-chat-notifiche-margin">
+            <li>
                 <a href="notifiche-home.php">
                     <img class="imgdatichat" src="<?php echo UPLOAD_DIR.'notification.png'?>" alt="notifiche">
                     <p>Dati Personali</p> 
                 </a>
             </li>
 
-            <li class="menu-chat-notifiche-margin">
+            <li>
                 <a href="index.php">
                     <img class="imgdatichat" src="<?php echo UPLOAD_DIR.'undo.png'?>" alt="esci">    
                     <p>Esci</p> 
--- template/venditore/account.php
+++ template/venditore/account.php
@@ -1,18 +1,21 @@
 <section>
     <h1>Il tuo account</h1>
 
-    <div>
+    <div class="vertical-items">
         <ul>
             <li>
-                <img src="<?php echo UPLOAD_DIR.'calendar.png'?>" alt="calendario impegni">
+                <img class= "imgdatichat" src="<?php echo UPLOAD_DIR.'calendar.png'?>" alt="calendario impegni">
                 <a href="">I miei impegni</a>
             </li>
-            <li>
-                <img src="<?php echo UPLOAD_DIR.'user.png'?>" alt="i tuoi dati">
+            <li >
+                <img class= "imgdatichat" src="<?php echo UPLOAD_DIR.'user.png'?>" alt="i tuoi dati">
                 <a href="">Dati personali</a>
             </li>
             <li>
-                <a href="">Esci</a>
+                <a href="index.php">
+                    <img class="imgdatichat" src="<?php echo UPLOAD_DIR.'undo.png'?>" alt="esci">    
+                    <p>Esci</p> 
+                </a>
             </li>
         </ul>
     </div>
