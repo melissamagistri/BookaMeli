e146aa1859adfef896cba00d9d817ad71a6de758
Melissa Magistri <melissamagistri@MacBook-Air-di-Melissa.local>
2022-03-21 15:05:21 +0100

    tolto css del rating

--- template/ordini.php
+++ template/ordini.php
@@ -26,17 +26,17 @@
             <div class="display-inline">
                 <p>Scegli una valutazione al prodotto:</p>
 
-                <div class="rate">
-                <input type="radio" id="star5" name="rate" value="5" />
-                <label for="star5" title="text">5 stelle</label>
-                <input type="radio" id="star4" name="rate" value="4" />
-                <label for="star4" title="text">4 stelle</label>
+                <div>
+                <input type="radio" id="star1" name="rate" value="5" />
+                <label for="star1" title="text">1 stella</label>
+                <input type="radio" id="star2" name="rate" value="4" />
+                <label for="star2" title="text">2 stelle</label>
                 <input type="radio" id="star3" name="rate" value="3" />
                 <label for="star3" title="text">3 stelle</label>
-                <input type="radio" id="star2" name="rate" value="2" />
-                <label for="star2" title="text">2 stelle</label>
-                <input type="radio" id="star1" name="rate" value="1" />
-                <label for="star1" title="text">1 stelle</label>
+                <input type="radio" id="star4" name="rate" value="2" />
+                <label for="star4" title="text">4 stelle</label>
+                <input type="radio" id="star5" name="rate" value="1" />
+                <label for="star5" title="text">5 stelle</label>
                 </div>
 
                 <div class="search">
