53d98e9e16384284ab16171694d5cc7259726c98
Federico <federico.guidazzi@studio.unibo.it>
2022-03-24 21:11:51 +0100

    risolto il problema della visualizzazione dello sconto dei prodotti nel carrello

--- template/base.php
+++ template/base.php
@@ -70,9 +70,9 @@
                                 </div>
                             </li>
                             <?php //sconto non appare //////////////////////////////////////////////////////////////////////////?>
-                            <li><p <?php echo ($prodotto['sconto'] != 0) ?"style : 'text-decoration:line-through;'" : '';?>> <?php echo $prodotto['prezzo']?></p></li>
+                            <li><p style=' <?php echo ($prodotto['sconto'] != 0) ? "text-decoration:line-through;" : '';?>'> <?php echo $prodotto['prezzo']?></p></li>
                             <?php if($prodotto['sconto'] != 0):?>
-                                <li><p><?php round($prodotto['prezzo'] - ($prodotto['prezzo']*$prodotto['sconto']/100),2,PHP_ROUND_HALF_UP).'€'?></p></li>
+                                <li><p><?php echo round($prodotto['prezzo'] - ($prodotto['prezzo']*$prodotto['sconto']/100),2,PHP_ROUND_HALF_UP).'€'?></p></li>
                             <?php endif; ?>
                             <li><button class="bluebutton">Rimuovi</button></li>
                         </ul>
