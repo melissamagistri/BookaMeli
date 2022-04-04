33e4c1cd03fb697b351947284425cfa2f000a957
Federico <federico.guidazzi@studio.unibo.it>
2022-04-04 14:19:43 +0200

    rimossa una stampa

--- js/listaprodotti.js
+++ js/listaprodotti.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     //parte per mettere la barra sul prezzo vecchio se c'è lo sconto.
     for(let i = 1; i<=$('.prodotti').length;i++){
         if($($(".prodotti:nth-of-type("+i+")>div >div:nth-of-type(1)")).children().length == 2){
-            console.log('si');
             $(".prodotti:nth-of-type("+i+")>div >div:nth-of-type(1)>p:nth-of-type(1)").css({'text-decoration': 'line-through', 'font-size':'10px'});
         };
     }
