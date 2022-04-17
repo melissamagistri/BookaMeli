ffe08303edd60acb2799d3d221378a92d64deb96
Melissa Magistri <melissamagistri@192.168.1.66>
2022-04-17 12:04:45 +0200

    sistemato css checkout

--- template/checkout.php
+++ template/checkout.php
@@ -1,28 +1,45 @@
 <?php if(isset($templateParams['errore'])): ?>
-    <p> <?php echo $templateParams['errore'] ?></p>
+    <p class="textcenter"> <?php echo $templateParams['errore'] ?></p>
 <?php endif; ?>
 
 <section class="border sectioncheckout">
     
+    <div style="gap: 4em;
+    display: flex;
+    flex-direction: column;">
+        <div>
             <h1 class="h1checkout">1.Informazioni Personali</h1>
-            <div class="inline-block">
-                <p>Nome:  <?php echo $templateParams["userinfo"][0]["nome"]; ?> </p>
-                <p>Cognome: <?php echo $templateParams["userinfo"][0]["cognome"]; ?></p>
-                <p>Email: <?php echo $templateParams["userinfo"][0]["email"]; ?> </p>
-            </div>
-    
-</section>
-
-<section class="border displayflexcenter">
-    <h1 class="h1checkout">2.Pagamento</h1>
-    <div class="textcenter no-padding inline-block">
-        <form class="form" action="#" method="POST">
-        <div class="base-list-style">
-            <input type="radio" id="contanti" name="metodo_pagamento" value="Contanti">
-            <label for="contanti">Contanti</label>
+        </div>
+        <div>
+            <h1 class="h1checkout">2.Pagamento</h1>
+        </div>
         
-            <input type="radio" id="carta" name="metodo_pagamento" value="Carta di credito">
-            <label for="carta">Carta di credito</label>
+    </div>
+            
+    <div style="    display: flex;
+    flex-direction: column;
+    gap: 4em;">
+        <div class="inline-block">               
+            <p>Nome:  <?php echo $templateParams["userinfo"][0]["nome"]; ?> </p>
+            <p>Cognome: <?php echo $templateParams["userinfo"][0]["cognome"]; ?></p>
+            <p>Email: <?php echo $templateParams["userinfo"][0]["email"]; ?> </p>
+        </div>
+
+        <div class="textcenter no-padding inline-block">
+        <form class="form" action="#" method="POST">
+            
+        <div class="base-list-style" style="display: flex;
+    flex-direction: column;">
+            <div style="display: inline-flex;">
+                <input type="radio" id="contanti" name="metodo_pagamento" value="Contanti">
+                <label for="contanti">Contanti</label>
+            </div>
+            
+            <div style="display: inline-flex;">
+                <input type="radio" id="carta" name="metodo_pagamento" value="Carta di credito">
+                <label for="carta">Carta di credito</label>
+            </div>
+            
         </div>
 
         
@@ -41,10 +58,13 @@
         </div>
 
                  
-        <div><button class="bluebutton submit">Continua</button></div>
+        <div style="display: grid;"><button class="bluebutton submit">Continua</button></div>
         </form>
 
 
+        </div>
+
+        
     </div>
-    
+  
 </section>
