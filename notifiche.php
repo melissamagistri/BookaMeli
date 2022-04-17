6f6a66d4b97b10443a17029fecc4dd404e599390
Melissa Magistri <melissamagistri@192.168.1.66>
2022-04-17 11:07:59 +0200

    Eliminata pagina delle notifiche

--- notifiche.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'util.php';
-
-$templateParams["titolo"] = "BookaMeli - Notifiche";
-$templateParams["nome"] = "template/notifiche-home.php";
-$templateParams["js"] = array("js/jquery-3.4.1.min.js","js/baseScript.js");
-
-require_once 'template/base.php';
-?>
