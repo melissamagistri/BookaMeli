72cae68b7a07c0fd902cca7755b68a3529066d8e
Federico <federico.guidazzi@studio.unibo.it>
2022-04-22 10:42:33 +0200

    iniziato a ristrutturare la schermata delle chat, sistemato il fatto che se non si avevano notifiche si visualizzava una pagina vuota

--- chat-home.php
+++ chat-home.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'util.php';
 
-$templateParams["titolo"] = "BookaMeli - Notifiche";
+$templateParams["titolo"] = "BookaMeli - Chat con il venditore";
 $templateParams["nome"] = "template/chat-home.php";
 $templateParams["js"] = array("js/jquery-3.4.1.min.js","js/baseScript.js");
 
