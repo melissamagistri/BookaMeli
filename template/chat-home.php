245a6cca3fa84c936a2aee45b6db3d92250333d8
Federico <federico.guidazzi@studio.unibo.it>
2022-04-25 16:35:05 +0200

    modificata la grafica delle chat

--- template/chat-home.php
+++ template/chat-home.php
@@ -27,15 +27,85 @@
 					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
 				</div>
 			</li>
-			<li>
-				<div class='writeMessage'>
-					<form action="">
-					<textarea placeholder="Type your message"></textarea>
-					<button class='bluebutton'><img src="<?php echo UPLOAD_DIR."send.png"; ?>" alt="invio messaggio" class='imgdatichat'></button>
-					</form>	
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
+				</div>
+			</li>
+			<li class="me">
+				<div class="entete">
+					<p class='chatElement'>Vincent</p>
+					<p class='chatElement'>10:12AM, Today</p>
+				</div>
+				<div class="message">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
 				</div>
 			</li>
 		</ul>
+		<div class='writeMessage'>
+			<form action="">
+				<textarea placeholder="Type your message"></textarea>
+				<button class='bluebutton'><img src="<?php echo UPLOAD_DIR."send.png"; ?>" alt="invio messaggio" style='width:25px'></button>
+			</form>	
+		</div>
 		
 		
 	</div>
