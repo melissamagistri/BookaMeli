8dac409239c49f57ffe38874a71f90be50c70291
Melissa Magistri <melissamagistri@192.168.1.66>
2022-04-25 17:53:30 +0200

    Aggiunto js sconto e nuovi elementi per gestione categorie

--- template/venditore/categorie.php
+++ template/venditore/categorie.php
@@ -2,9 +2,20 @@
     <h1 class="padding20">Scegli quale azione vuoi eseguire: </h1>
 
 <div class="padding20 displayflexcenter gap2">
-    <button class="bluebutton" onclick="window.location.href='modifica-categoria.php'">Elimina o Modifica Categoria</button>
+    <button class="bluebutton" onclick="window.location.href='modifica-categoria.php'">Elimina Categoria</button>
     <button class="bluebutton" onclick="window.location.href='nuova-categoria.php'">Aggiungi nuova Categoria</button>
 </div>
 
+<div class="aggiungicategoria">
+    <input type="text">
+    <button>Aggiungi</button>
+</div>
+
+<div>
+    <select name="" id="">
+        <option value=""></option>
+    </select>
+    <button>Elimina</button>
+</div>
 
 </section>
