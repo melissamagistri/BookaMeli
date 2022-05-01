f350337a865dfa87c742b663afd949e61db2b272
Federico <federico.guidazzi@studio.unibo.it>
2022-05-01 18:05:18 +0200

    rimosso una print di una variabile

--- template/confermaOrdine.php
+++ template/confermaOrdine.php
@@ -1,4 +1,3 @@
 <section>
-    <?php var_dump($templateParams["account"]) ?>
     <p>Grazie, il tuo ordine è stato ricevuto con successo. Ti abbiamo spedito una mail con maggiori informazioni riguardanti l'ordine, puoi visualizzare lo stato dell'ordine nella schermata 'I miei ordini' nel tuo profilo.</p>
 </section>
