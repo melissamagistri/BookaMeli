956dd64a614738574f5e215d55b80699ce09e01f
Melissa Magistri <melissamagistri@Air-di-Melissa.homenet.telecomitalia.it>
2022-05-26 19:18:22 +0200

    css avvisi

--- template/avvisi.php
+++ template/avvisi.php
@@ -1,9 +1,9 @@
 <section>
     <?php if(count($templateParams['avvisi']) > 0):?>
-    <ul>
-        <?php foreach($templateParams['avvisi'] as $avviso)?>
-        <li>
-            <a class='image' href="prodotto.php?foto=<?php echo $avviso['foto']?>">
+    <ul class="no-padding displayflex">
+        <?php foreach($templateParams['avvisi'] as $avviso):?>
+        <li class="no-margin textcenter prodotti">
+            <a class='image flex-a' href="prodotto.php?foto=<?php echo $avviso['foto']?>">
                 <div>
                     <img class='imgordini' src="<?php echo UPLOAD_DIR.$avviso['foto']?>" alt="<?php echo $avviso['nome']?>">
                     <p><?php echo $avviso['nome']?></p>
@@ -12,8 +12,8 @@
             <form action="" method="get">
                 <button class="bluebutton" name="rimuoviavviso" value="<?php echo $avviso['idprodotto']?>">Rimuovi</button>
             </form>
-            
         </li>
+        <?php endforeach;?>
     </ul>
     <?php else:?>
         <p>Non hai ancora inserito nessun prodotto per il quale vuoi essere avvisato.</p>
