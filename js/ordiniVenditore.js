a81ba8c9e8a51d5d4cf942a0984acbf68d8b5e1a
Melissa Magistri <melissamagistri@192.168.1.65>
2022-05-30 18:42:56 +0200

    risolto bug in ordini venditore in cui non si poteva aggiornare lo stato della consegna

--- js/ordiniVenditore.js
+++ js/ordiniVenditore.js
@@ -1,36 +1,40 @@
 $(document).ready(function () {
-    $(".radiobuttonattivo").attr("checked", "true");
+  $(".radiobuttonattivo").attr("checked", "true");
 
-    $(".listaProdotti").hide();
-  
-    $(".divlistaProdotti").click(function () {
-      if ($(this).hasClass("selezionato")) {
-        hideElement($(this), "selezionato");
-      } else {
-        hideElement($(".selezionato"), "selezionato");
-        $(this).addClass("selezionato").next().slideDown();
-      }
-    });
+  $(".listaProdotti").hide();
 
-    $('#statoOrdine').on('change', function() {
-        var statoOrdine = {
-            //mettere a posto le variabili che passo
-            idordine: $('.selezionato > p:last-of-type()').text() ,
-            stato: this.value
-        };
-        
-        $.post('aggiornaStatoOrdine.php',{
-            statoOrdine: statoOrdine
-        }, function(data, success){
-            if(statoOrdine['stato'] == 'consegnato'){
-              $('.selezionato > p:nth-of-type(3)').text('Stato: Consegnato');
-            } else {
-              $('.selezionato > p:nth-of-type(3)').text('Stato: Non ancora spedito');
-            }
-        });
-    });
-    
+  $(".divlistaProdotti").click(function () {
+    if ($(this).hasClass("selezionato")) {
+      hideElement($(this), "selezionato");
+    } else {
+      hideElement($(".selezionato"), "selezionato");
+      $(this).addClass("selezionato").next().slideDown();
+    }
   });
+
+  $("select").on("change", function () {
+    var statoOrdine = {
+      idordine: $(".selezionato > p:last-of-type()").text(),
+      stato: this.value,
+    };
+
+    $.post(
+      "aggiornaStatoOrdine.php",
+      {
+        statoOrdine: statoOrdine,
+      },
+      function (data, success) {
+        if (statoOrdine["stato"] == "consegnato") {
+          $(".selezionato > p:nth-of-type(3)").text("Stato: Consegnato");
+        } else {
+          $(".selezionato > p:nth-of-type(3)").text(
+            "Stato: Non ancora spedito"
+          );
+        }
+      }
+    );
+  });
+});
 function hideElement(element, string) {
-    element.removeClass(string).next().slideUp();
-  }
+  element.removeClass(string).next().slideUp();
+}
--- template/venditore/ordini.php
+++ template/venditore/ordini.php
@@ -17,19 +17,22 @@
             <ul class='listaProdotti textcenter padding-bottom'>
                 <?php foreach($templateParams['prodottiinOrdine'][$i] as $ordine):?>
                 <li class="padding-bottom">
-                    <a class='image padding-bottom5' href="prodotto.php?foto=<?php echo $ordine['foto']?> ">
+                    <a class='image padding-bottom5' href="visualizza_prodotto.php?foto=<?php echo $ordine['foto']?> ">
                         <img class='imgordini padding-bottom10' src="<?php echo UPLOAD_DIR.$ordine['foto']?>" alt="<?php echo $ordine['nome']?>">
                         <div class="padding-bottom5"><?php echo $ordine['nome']?></div>
                     </a>
                     <p class="padding-bottom5">Quantità: <?php echo $ordine['quantita']?></p>
                     <p class="padding-bottom5">Prezzo: <?php echo $ordine['costo']?></p>
-                    <label for="statoOrdine">Cambia stato Ordine:</label>
-                    <select class="padding-bottom5 select" name="" id="statoOrdine">
-                        <option <?php echo ($templateParams['ordini'][$i]['stato']) == 0 ? '' : 'selected'?> value="consegnato">Consegnato</option>
-                        <option <?php echo ($templateParams['ordini'][$i]['stato']) == 0 ? 'selected' : ''?> value="nonAncoraSpedito">Non ancora spedito</option>
-                    </select>    
+                       
                 </li>
                 <?php endforeach;?>
+                <li>
+                    <label for="statoOrdine<?php echo $templateParams['ordini'][$i]['idordine']?>">Cambia stato Ordine:</label>
+                    <select class="padding-bottom5 select" name="" id="statoOrdine<?php echo $templateParams['ordini'][$i]['idordine']?>">
+                        <option <?php echo ($templateParams['ordini'][$i]['stato']) == 0 ? '' : 'selected'?> value="consegnato">Consegnato</option>
+                        <option <?php echo ($templateParams['ordini'][$i]['stato']) == 0 ? 'selected' : ''?> value="nonAncoraSpedito">Non ancora spedito</option>
+                    </select> 
+                </li>
             </ul>
         </li>
         <?php endfor;?>
